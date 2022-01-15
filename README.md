9e19baf1f5604318f02467b5004b7dd4f71014eb
Aaron Jackson <aaronjackson@Aarons-Mac-mini.attlocal.net>
2022-01-15 11:58:17 -0800

    chore: removing bad code. bump version 1.0.0 -> 1.0.1

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Includes
 * Typescript
 * Eslint
 * Nodemon
+
+# Install
+
+Install dependencies
+
+`npm i`
+
+# Testing
+
+Launch your program and changes to your code will be reflected on save thanks to nodemon.
+
+`npm start`
--- index.ts
+++ index.ts
@@ -1,12 +1,1 @@
-import express from 'express';
-
-const app = express();
-const port = 3000;
-
-app.post('/read', (req, res) => {
-    
-});
-
-app.listen(port, () => {
-    return console.log(`Express listening on port: ${port}`);
-});
+// Your code here
